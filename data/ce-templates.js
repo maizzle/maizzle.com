0859bd1bf7fe2c0d10ea3a2c062b0e939ff47898
Cosmin Popovici <hellocosmin@gmail.com>
2023-08-04 13:38:49 +0300

    wip

---
+++ data/ce-templates.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    name: "Flows",
+    description: "Auth & secure login emails",
+    url: "https://craftingemails.com/flows-auth-secure-login-email-templates",
+    image: "https://res.cloudinary.com/dxa66e5ic/image/upload/w_768,h_574,c_thumb,f_auto,q_auto:best/dpr_1.0/v1635689244/website/landings/hero-images/auth-email.png",
+  },
+  {
+    name: "Essential",
+    description: "eCommerce emails",
+    url: "https://craftingemails.com/ecommerce-email-templates",
+    image: "https://res.cloudinary.com/dxa66e5ic/image/upload/w_768,h_574,c_thumb,f_auto,q_auto:best/dpr_1.0/v1635689244/website/products/thumbnails/essential-commerce-thumbnail.jpg",
+  },
+  {
+    name: "Slick",
+    description: "Welcome & Onboarding emails",
+    url: "https://craftingemails.com/slick-welcome-email-templates",
+    image: "https://res.cloudinary.com/dxa66e5ic/image/upload/w_768,h_574,c_thumb,f_auto,q_auto:best/dpr_1.0/v1635689244/website/products/thumbnails/slick-welcome-thumbnails.jpg",
+  },
+]
