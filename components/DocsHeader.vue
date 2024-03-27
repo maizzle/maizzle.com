ee21ca219999151cbdb80825a426ae232603bd4c
Cosmin Popovici <hellocosmin@gmail.com>
2024-03-27 13:45:24 +0200

    feat: update docs nav

--- components/DocsHeader.vue
+++ components/DocsHeader.vue
@@ -185,8 +185,8 @@
       class="fixed lg:hidden top-0 w-64 sm:w-96 h-full bg-white z-[60] overflow-auto transition-transform"
       :class="[showMobileMenu ? 'translate-x-0 shadow-2xl' : '-translate-x-full']"
     >
-      <div class="p-4 sm:p-8 space-y-6">
-        <div class="flex gap-2 justify-between items-center">
+      <div class="p-4 sm:p-8">
+        <div class="flex gap-2 justify-between items-center relative z-10">
           <NuxtLink to="/" aria-label="Maizzle">
             <InsigniaSvg class="w-10" />
           </NuxtLink>
@@ -199,6 +199,8 @@
             </svg>
           </button>
         </div>
+        <!-- Top fade -->
+        <div class="sticky top-0 bg-slate-5s00 h-10 w-full -mt-4 bg-gradient-to-b from-white pointer-events-none z-0"></div>
         <ul class="mb-4 -ml-4 text-sm sm:text-base sm:leading-7">
           <li>
             <NuxtLink
@@ -269,6 +271,28 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
               </svg>
               Resources
+            </NuxtLink>
+          </li>
+          <li>
+            <a
+              href="https://maizzle.com/discord"
+              class="flex gap-2 items-center px-4 py-2 text-slate-500"
+            >
+              <svg class="h-6 w-6 text-slate-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z" />
+              </svg>
+              Community
+            </a>
+          </li>
+          <li>
+            <NuxtLink
+              to="/sponsors"
+              class="flex gap-2 items-center px-4 py-2 text-slate-500"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-slate-400">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+              </svg>
+              Sponsors
             </NuxtLink>
           </li>
         </ul>
